0002d9fb7e1b16291d3d75c121004fac292a60be
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-09-27 02:31:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,14 @@
 def game_hash
-  {}
+  {
+    home: {
+      team_name: nil,
+      colors: nil,
+      players: nil
+    },
+    away: {
+      team_name: nil,
+      colors: nil,
+      players: nil
+    }
+  }
 end
